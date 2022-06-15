98880a54a71bf5425381865642e8b669dff30022
ikechukwu <ikechukwu@brandmobile.net>
2022-06-15 17:01:08 +0100

    ft-added-detail-pages

--- src/components/primary/DetailPages.vue
+++ src/components/primary/DetailPages.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     hello
   </div>
 </template>
@@ -10,6 +10,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.container {
+height: 100vh;
+}
 </style>
